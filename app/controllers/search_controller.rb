4922655d743abfda4a3cf48d6e595bd2370d73d0
Evan Kozliner <evankozliner@gmail.com>
2014-07-21 11:21:30 -0400

    Basic 'on enter' functionality for rotten tomatoes queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,7 +10,6 @@ class SearchController < ApplicationController
 
     if params[:type] == "entries"
       @results = Entry.search query
-
       #TODO: Switch this to bisection for speed
       if params[:t] == 'tomatoes'
         has_entry = false
@@ -21,17 +20,21 @@ class SearchController < ApplicationController
         end
         #TODO: (in progress) finish this method
         unless has_entry
+          entry = false
           #query rotten tomatoes
-          uri = URI("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=3gh3a35gm2x9x3as2qp69rr4&q=" + query + "&page_limit=1")
+          uri = URI("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" +
+          ENV["ROTTEN_TOMATOES_KEY"] + "&q=" + query.to_s.gsub(" ", "+") + "&page_limit=1")
           resp = JSON.parse(Net::HTTP.get_response(uri).body)
-          unless reps.blank?
+          unless resp.blank?
             #add each resp to the database
-            Entry.create(title: , description: )
-            #append to results
+            resp["movies"].each do |movie|
+              entry = Entry.create(title: movie["title"], description: movie["synopsis"])
+            end
+            #there's certainly a better way of doing this...
+            @results = Entry.search query
           end
         end
       end
-
     else
       @results = User.search query
     end
@@ -39,6 +42,7 @@ class SearchController < ApplicationController
     if @results.blank? && @results.count > 0
       render nothing: true
     else
+      # if has_entry then render json: @results else render json: @results.to_a.push(entry).to_json end
       render json: @results
     end
   end
