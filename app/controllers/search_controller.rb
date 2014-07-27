9543ed9f8d3868e669e0cda3a36e66fb7892c8f5
Evan Kozliner <evankozliner@gmail.com>
2014-07-27 18:48:25 -0400

    the dashboard search is now operational

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,46 +4,52 @@ class SearchController < ApplicationController
   def index
     logger = Logger.new('log/development.log')
     query = params[:query]
-    #TODO: Switch this to bisection for speed
-    #TODO: handle /search/entries//none
-    if query.blank?
-      Entry.reindex
-      User.reindex
-      #TODO: add a message instead
-      @results = Entry.search query, page: params[:page], per_page: 10
-    else
-      @results = Entry.search query
-      has_entry = false
-      @results.to_a.each do |result|
-        #TODO: this check needs to be stronger to prevent duplicate queries
-        if result.title.downcase.gsub(/\s+/, "") == query.downcase.gsub(/\s+/, "")
-          has_entry = true
-        end
-      end
-      #TODO: (in progress) finish this method
-      unless has_entry
-        entry = false
-        uri = URI("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" +
-        ENV["ROTTEN_TOMATOES_KEY"] +
-        "&q=" +
-        query.to_s.gsub(" ", "+") +
-        "&page_limit=5")
-        resp = JSON.parse(Net::HTTP.get_response(uri).body)
-        unless resp.blank?
-          resp["movies"].each do |movie|
-            if movie["alternate_ids"].blank? then imdb = nil else imdb = movie["alternate_ids"]["imdb"] end
-            Entry.create(
-            title: movie["title"],
-            description: movie["synopsis"],
-            imdb_id: imdb
-            )
+    @group = params[:group]
+    if @group == 'entries'
+      #TODO: Switch this to bisection for speed
+      #TODO: handle /search/entries//none
+      if query.blank?
+        Entry.reindex
+        User.reindex
+        #TODO: add a message instead
+        @results = Entry.search query, page: params[:page], per_page: 10
+      else
+        @results = Entry.search query
+        has_entry = false
+        @results.to_a.each do |result|
+          #TODO: this check needs to be stronger to prevent duplicate queries
+          if result.title.downcase.gsub(/\s+/, "") == query.downcase.gsub(/\s+/, "")
+            has_entry = true
           end
         end
+        #TODO: (in progress) finish this method
+        unless has_entry
+          entry = false
+          uri = URI("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" +
+          ENV["ROTTEN_TOMATOES_KEY"] +
+          "&q=" +
+          query.to_s.gsub(" ", "+") +
+          "&page_limit=20")
+          resp = JSON.parse(Net::HTTP.get_response(uri).body)
+          unless resp.blank?
+            resp["movies"].each do |movie|
+              if movie["alternate_ids"].blank? then imdb = nil else imdb = movie["alternate_ids"]["imdb"] end
+              Entry.create(
+              title: movie["title"],
+              thumbnail_file_name: movie["posters"]["thumbnail"],
+              description: movie["synopsis"],
+              imdb_id: imdb
+              )
+            end
+          end
+        end
+        #TODO: there's certainly a better way of doing this... (not DRY, we query twice)
+        #Seems to fix the issue
+        sleep 0.25
+        @results = Entry.search query, page: params[:page], per_page: 10
       end
-      #TODO: there's certainly a better way of doing this... (not DRY, we query twice)
-      #Seems to fix the issue
-      sleep 0.25
-      @results = Entry.search query, page: params[:page], per_page: 10
+    else
+      @results = User.search query
     end
   end
   def query
