031c607e7355f9e40f7d5b399586ba20dff966c2
Evan Kozliner <evankozliner@gmail.com>
2015-01-30 14:41:51 -0500

    some starting code for displaying zaps

--- app/controllers/zaps_controller.rb
+++ app/controllers/zaps_controller.rb
@@ -1,2 +1,11 @@
 class ZapsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    if current_user.zaps.nil?
+      @zaps = []
+    else
+      @zaps = current_user.zaps
+    end
+  end
 end
