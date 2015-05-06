057ff024432b60c119530067f63af753266264d8
Evan Kozliner <evankozliner@gmail.com>
2015-05-06 16:11:56 -0400

    small settings error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'entries/remove/:id'   => 'entries#delete'
   get 'entries/add/:id'      => 'entries#add'
   get '/entries/'            => 'entries#new'
-  get 'users/edit'           => 'users#edit'
+  # get 'users/edit'           => 'users#edit'
   get 'users/followers'      => 'users#followers'
   get 'users/following'      => 'users#following'
   get 'users/follow/:id'     => 'users#follow'
