6418b3af48edcf027421a9631ed5fbc0a4ea95ef
Evan Kozliner <evankozliner@gmail.com>
2015-06-12 09:15:31 -0700

    working on adding the proper query for rotten tomatoes, I'll need to add the rotten tomatoes id. Probably need a rake task too for existing entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -42,28 +42,70 @@ class Entry < ActiveRecord::Base
   end
 
   #http://api.rottentomatoes.com/api/public/v1.0/movies/770672122.json?apikey=3gh3a35gm2x9x3as2qp69rr4
-  def get_feed(endpoint)
+  def self.get_feed(endpoint)
     resp = Net::HTTP.get_response(URI.parse(endpoint))
     data = resp.body
     feed = JSON.parse(data)
     return feed
   end
-end
 
-class UniquenessValidator < ActiveModel::Validator
-  def bare s
-    return s.gsub(/[-()'*,.]/, "").strip.downcase
+  # Create a "coordinate" by querying rotten tomatoes and making an object
+  # We must query then loop here, as Rotten Tomatoes does not provide a find imdb_id
+  # STORE ROTTEN TOMATOES ID
+  def coordinate_attributes(imdb_id, title)
+    attributes = {}
+    endpoint = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" +
+      ENV["ROTTEN_TOMATOES_KEY"] + "&q=" + title.gsub(" ", "+") + "&page_limit=20"
+    resp = self.get_feed(endpoint)
+    found_movie = false
+    unless resp.blank?
+      count = 0
+      while !found_movie && count < resp["movies"].size do
+        movie = resp["movies"][count]
+        if movie["alternate_ids"]["imdb"] == imdb_id
+          found_movie = true
+          # attributes["directors"] =
+        end
+        count += 1
+      end
+    end
+    return attributes
   end
-  def validate(entry)
-    entries = Entry.where(title: entry.title)
-    if entries.count > 0
-      descriptions = entries.pluck(:description)
-      test_description = bare(entry.description)
-      descriptions.each do |description|
-        if bare(description) == bare(test_description)
-          entry.errors[:base] << "Duplicate title/description parir"
+
+  # Inner classes
+
+  # A coordinate for an entry to be used in suggestions
+  class DataPoint
+    def initialize(entry)
+      attrs = coordinate_attributes(entry.imdb_id, entry.title)
+    end
+
+    # Computes the "distance" between entries as an integer, x, such that 0 <= x <= 3
+    # Uses the directors (3), genre (1*num), actors(1*num), mpaa rating(2) and studio(3)
+    # to determine similarity between points. Weights for each are in parenthesis.
+    def distance(point)
+      distance = 0
+      entry_coordinate = generate_coordinate
+      return distance
+    end
+  end
+
+  class UniquenessValidator < ActiveModel::Validator
+    def bare s
+      return s.gsub(/[-()'*,.]/, "").strip.downcase
+    end
+    def validate(entry)
+      entries = Entry.where(title: entry.title)
+      if entries.count > 0
+        descriptions = entries.pluck(:description)
+        test_description = bare(entry.description)
+        descriptions.each do |description|
+          if bare(description) == bare(test_description)
+            entry.errors[:base] << "Duplicate title/description parir"
+          end
         end
       end
     end
   end
+
 end
