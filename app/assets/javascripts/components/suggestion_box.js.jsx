0d293abbdc92a01793d05d9112490543274e28e6
Evan Kozliner <evankozliner@gmail.com>
2015-07-25 17:00:28 -0700

    Display rotten tomatoes suggestions

--- app/assets/javascripts/components/suggestion_box.js.jsx
+++ app/assets/javascripts/components/suggestion_box.js.jsx
@@ -1,11 +1,26 @@
+var SuggestionListItem = React.createClass({
+  quickAdd: function(e) {
+    console.log(e);
+  },
+  render: function() {
+    return (
+      <li><a href='#' onClick={this.quickAdd}>
+      {this.props.suggestionData['title']}
+      </a></li>
+    );
+  }
+});
+
 var SuggestionList = React.createClass({
   render: function() {
-    var suggestions = [];
+    var suggestionList = [];
     function iterateSuggestions(suggestions) {
-
+      for(var suggestionIndex in suggestions) {
+        suggestionList.push(<SuggestionListItem suggestionData={suggestions[suggestionIndex]} />)
+      }
     }
     iterateSuggestions(this.props.suggestionList);
-    return <ul className='suggestion-list'>{suggestions}</ul>;
+    return <ul className='suggestion-list'>{suggestionList}</ul>;
   }
 });
 
@@ -15,18 +30,12 @@ var GuessListItem = React.createClass({
     var imdbId = this.props.movieData['imdb_id'];
     if (rtId !== null) {
       $.get('/rt_suggestion/rt/' + rtId, function(data) {
-        console.log(data);
-        // this.setState({suggestions: ['my', 'rt', 'suggestions']});
-        this.props.suggest(['my', 'rt', 'suggestions']);
+        this.props.suggest(data["movies"]);
       }.bind(this));
     } else if (imdbId !== null) {
       $.get('/rt_suggestion/imdb/' + imdbId, function(data) {
-        console.log(data);
-        // this.setState({suggestions: ['my', 'rt', 'suggestions']});
-        this.props.suggest(['my', 'rt', 'suggestions']);
+        this.props.suggest(data["movies"]);
       }.bind(this));
-    } else {
-      console.log("nada");
     }
   },
   render: function() {
@@ -46,7 +55,6 @@ var GuessList = React.createClass({
         items.push(<GuessListItem movieData={data[index]} suggest={suggestFunction}/>);
       }
     }
-    // this.setState(suggestions:)
     iterateData(this.props.guessList, this.props.suggest);
     return <ul className='guess-list'>{items}</ul>;
   }
@@ -72,15 +80,12 @@ var SuggestionBox = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
   },
-  suggestionClick: function(e) {
-    console.log(e);
-  },
   render: function() {
     return (
       <div>
         <div className="col-lg-6" >
           <h3>Suggestions</h3>
-          <form onSubmit={this.handleSubmit} className=''>
+          <form className=''>
             <input onChange={this.onChange} value={this.state.text} />
             <button className="btn-suggest" onClick={this.suggestionClick}>
               {'Suggestions based on ' + (this.state.text)}
